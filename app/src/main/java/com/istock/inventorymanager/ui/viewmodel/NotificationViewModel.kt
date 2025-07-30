fa6faf017a5aa40ad9a50492c9bfa4644f67a406
axp9210 <axp9210@mavs.uta.edu>
2025-07-30 12:37:32 -0500

    implement notification

--- app/src/main/java/com/istock/inventorymanager/ui/viewmodel/NotificationViewModel.kt
+++ app/src/main/java/com/istock/inventorymanager/ui/viewmodel/NotificationViewModel.kt
@@ -3,12 +3,15 @@ package com.istock.inventorymanager.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.istock.inventorymanager.data.model.Notification
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.util.*
 
-class NotificationViewModel : ViewModel() {
+@HiltViewModel
+class NotificationViewModel @Inject constructor() : ViewModel() {
     private val _notifications = MutableStateFlow<List<Notification>>(emptyList())
     val notifications: StateFlow<List<Notification>> = _notifications
 
